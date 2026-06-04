16cea534f4b2d2e8efd2399728efbbf9fc0e49ac
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-06-04 22:38:41 +0530

    Update CorsConfig.java

--- src/main/java/com/vendora/project/security/CorsConfig.java
+++ src/main/java/com/vendora/project/security/CorsConfig.java
@@ -1,6 +1,7 @@
-package com.vendora.project.config;
+package com.vendora.project.security;
 
 import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,20 +14,29 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
+        CorsConfiguration config = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-            "https://full-stack-java-project-yqbm-j6rmgqquy.vercel.app"
+        config.setAllowedOrigins(List.of(
+            "https://full-stack-java-project-yqbm-j6rmgqquy.vercel.app",
+            "http://localhost:5173"
         ));
 
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        config.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", config);
 
         return source;
     }
