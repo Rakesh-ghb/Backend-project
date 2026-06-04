62e5d99c342264df8f4d54437441777297c74e55
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-06-04 22:56:41 +0530

    Update CorsConfig.java

--- src/main/java/com/vendora/project/security/CorsConfig.java
+++ src/main/java/com/vendora/project/security/CorsConfig.java
@@ -2,6 +2,7 @@ package com.vendora.project.security;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,27 +12,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        // React app origin
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(frontendUrl));
 
-        // Allowed HTTP methods
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
-        // Allowed headers
         config.setAllowedHeaders(List.of("*"));
 
-        // Allow sending JWT in headers
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        // Apply this CORS config to all endpoints
         source.registerCorsConfiguration("/**", config);
 
         return source;
